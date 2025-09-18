0ed7a5d94468c6413850193c6e715d971b626ca7
Nunzio11 <nunzio.de97@gmail.com>
2025-09-18 15:02:17 +0200

    Add files via upload

---
+++ LegaSeriea/src/main/java/org/legaseriea/repository/AllenatoreRepository.java
@@ -0,0 +1,8 @@
+package org.legaseriea.repository;
+
+import org.legaseriea.entity.Allenatore;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AllenatoreRepository extends JpaRepository<Allenatore, Long>{
+
+}
---
+++ LegaSeriea/src/main/java/org/legaseriea/repository/GiocatoreRepository.java
@@ -0,0 +1,8 @@
+package org.legaseriea.repository;
+
+import org.legaseriea.entity.Giocatore;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GiocatoreRepository extends JpaRepository<Giocatore, Long>{
+
+}
